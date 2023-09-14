80fe938f60f7ef86141e2c4c16a52f247f85ba33
buithien168 <thien7278@gmail.com>
2023-09-14 20:25:48 +0700

    thien fix map

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,8 @@
 @extends('pages.layouts.app')
 @section('title', 'Trang Chủ')
+@section('css')
+    <link rel="stylesheet" href="{{ asset('leaflet/leaflet.css') }}">
+@endsection
 @section('content')
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-section set-bg" data-setbg="{{ asset('user_/image/breadcrumb.jpg') }} ">
@@ -57,24 +60,11 @@
     <!-- Contact Section End -->
 
     <!-- Map Begin -->
-    {{-- <div id="map" style="height: 500px;"></div> --}}
     <div class="map">
-        <iframe id="map-iframe"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49116.39176087041!2d-86.41867791216099!3d39.69977417971648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886ca48c841038a1%3A0x70cfba96bf847f0!2sPlainfield%2C%20IN%2C%20USA!5e0!3m2!1sen!2sbd!4v1586106673811!5m2!1sen!2sbd"
-            height="500" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-        <div class="map-inside">
-            <i class="icon_pin"></i>
-            <div class="inside-widget">
-                <h4>Thái Nguyên</h4>
-                <ul>
-                    <li>Phone: +12-345-6789</li>
-                    <li class="as">Add: Nam Sơn - Linh Sơn - Thái Nguyên</li>
-                </ul>
-            </div>
+        <div>
+            <div class="leaflet-map" id="userLocation"></div>
         </div>
     </div>
-    <!-- Map End -->
-
     <!-- Contact Form Begin -->
     <div class="contact-form spad">
         <div class="container">
@@ -104,31 +94,29 @@
     <!-- Contact Form End -->
 @endsection
 @section('script')
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&callback=initMap" async defer>
-    </script>
+    <script src="{{ asset('leaflet/leaflet.js') }}"></script>
     <script>
-        function initMap() {
-            var mapDiv = document.getElementById('map');
-            var map = new google.maps.Map(mapDiv, {
-                center: {
-                    lat: 0,
-                    lng: 0
-                },
-                zoom: 8
+        const userLocationVar = document.getElementById('userLocation');
+        if (userLocationVar) {
+            const userLocation = L.map('userLocation').setView([42.35, -71.08], 10);
+            userLocation.locate({
+                setView: true,
+                maxZoom: 16
             });
 
-            // Thay thế vị trí mặc định bằng vị trí người dùng
-            var latitude = 21.583328;
-            var longitude = 105.8404793;
-
-            var userLocation = new google.maps.Marker({
-                position: {
-                    lat: latitude,
-                    lng: longitude
-                },
-                map: map,
-                title: 'Your Location'
-            });
+            function onLocationFound(e) {
+                const radius = e.accuracy;
+                L.marker(e.latlng)
+                    .addTo(userLocation)
+                    .bindPopup('You are somewhere around ' + radius + ' meters from this point')
+                    .openPopup();
+                L.circle(e.latlng, radius).addTo(userLocation);
+            }
+            userLocation.on('locationfound', onLocationFound);
+            L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+                maxZoom: 18
+            }).addTo(userLocation);
         }
     </script>
 @endsection
